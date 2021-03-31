e8e14bb342fbabc5a200fb10414b7b86a6f6e712
sallecta <sallecta@inbox.ru>
2021-03-31 20:30:58 +0300

    ...

--- 046_pointers/main.cpp
+++ 046_pointers/main.cpp
@@ -10,14 +10,22 @@ int main()
     //cout << "Hello world!" << endl;
 
     int a = 5;
+    cout << "a\t" << a << endl;
     int *px = &a;
     int *px2 = &a;
 
     cout << px << endl;
     cout << *px << endl;
 
-    cout << px << endl;
-    cout << px2 << endl;
+    cout << "px\t" << px << endl;
+    cout << "px2\t" << px2 << endl;
+    cout << "&a\t" << &a << endl;
+    cout << "*&a\t" << *&a << endl;
+
+    *px2 = 22;
+    cout << "a\t" << a << endl;
+    cout << "*px\t" << *px << endl;
+    cout << "*px2\t" << *px2 << endl;
 
     return 0;
 }
---
+++ 047_Pointer_arithmetic/main.cpp
@@ -0,0 +1,12 @@
+// https://www.youtube.com/watch?v=Weh6UoLkNUQ
+// Арифметика указателей. Указатели и массивы. Массив это указатель. C++ для начинающих. Урок #47
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
