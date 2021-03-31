2c27eb7314c5e1c0c530516bf76a41003db676e4
sallecta <sallecta@inbox.ru>
2021-03-31 18:24:42 +0300

    ...

---
+++ 130_smartPointers/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+template<typename T>
+class SmartPointer
+{
+public:
+    SmartPointer(T *argPtr)
+    {
+        std::cout<<"class SmartPointer.Constructor"<<std::endl;
+        this->ptr= argPtr;
+    }
+
+    ~SmartPointer()
+    {
+        std::cout<<"class SmartPointer.Destructor"<<std::endl;
+        delete this->ptr;
+    }
+
+    //operator * overload
+    // return link to T
+    T& operator*()
+    {
+      return *this->ptr;
+    }
+private:
+    T *ptr;
+};
+
+
+int main()
+{
+    //std::cout << "Hello world!" << std::endl;
+    int *ptr = new int(5);
+    std::cout<<ptr<<std::endl;
+    std::cout<<*ptr<<std::endl;
+
+    SmartPointer<int> pointer = new int(51);
+    //or
+    *pointer = 1121214;
+
+    std::cout<<*pointer<<std::endl;
+
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# SimpleCode-edu
+
+Some CodeBlocks adopted sources from [Основы C++. Программирование для начинающих](https://youtube.com/playlist?list=PLQOaTSbfxUtCrKs0nicOg2npJQYSPGO9r) video course by [#SimpleCode](https://www.youtube.com/channel/UCtLKO1Cb2GVNrbU7Fi0pM0w) YouTube channel.
